76c7da5c862f61fd827c7d13572d04ece9e9ba21
poker90102 <poker.king90102@gmail.com>
2015-08-18 01:26:43 +0800

    fix hot reload

--- app/stores/AppStore.js
+++ app/stores/AppStore.js
@@ -1,19 +1,18 @@
-import AppDispatcher from '../dispatcher/AppDispatcher';
-import ActionTypes from '../constants/ActionTypes';
-import createStore from '../utils/createStore';
+import AppDispatcher from '../dispatcher/AppDispatcher'
+import ActionTypes from '../constants/ActionTypes'
+import createStore from '../utils/createStore'
 
 
 export default createStore({
 
-});
+})
 
 
-AppDispatcher.register(payload => {
-  var action = payload.action;
+AppDispatcher.register(function(payload) {
+  var action = payload.action
   switch (action.type) {
-    case ActionTypes.ROUTE_CHANGE:
-      break;
+
     default:
 
   }
-});
+})
