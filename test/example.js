d646eaa490a23bf53042f3e26058c5d23b194b66
poker90102 <poker.king90102@gmail.com>
2015-08-31 09:36:03 +0800

    0.3.0 changelog

--- test/example.js
+++ test/example.js
@@ -1,9 +1,7 @@
-/* eslint func-names: 0, no-unused-vars: 0 */
-var expect = require('chai').expect;
+var chai = require('chai')
 
-
-describe('description', function() {
-  it('description', function() {
+describe('description', function () {
+  it('description', function () {
     // body...
-  });
-});
+  })
+})
